da876d79823f1e970d089132f1eb2bd144dc92dd
Md. Moniruzzaman (Office) <moon199715@gmail.com>
2020-12-15 17:42:01 +0600

    completed design of home page with footer

--- resources/js/components/includes/Footer.vue
+++ resources/js/components/includes/Footer.vue
@@ -4,37 +4,37 @@
 			<div class="icon-info-wrapper w-100">
 				<div class="container">
 					<div class="row w-100 m-auto icon-row text-center">
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 icon-col">
+						<div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 icon-col">
 							<div>
 								<img class="icon-img" src="/medias/icons/productAmount.png" alt="">
 							</div>
 							<span class="icon-info-title">2 Lakhs + Unique Products</span>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 icon-col">
+						<div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 icon-col">
 							<div>
 								<img class="icon-img" src="/medias/icons/categoryCount.png" alt="">
 							</div>
 							<span class="icon-info-title">2 Hundred + Products Category</span>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 icon-col">
+						<div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 icon-col">
 							<div>
 								<img class="icon-img" src="/medias/icons/brandNumber.png" alt="">
 							</div>
 							<span class="icon-info-title">5800 Brands</span>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 icon-col">
+						<div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 icon-col">
 							<div>
 								<img class="icon-img" src="/medias/icons/userCount.png" alt="">
 							</div>
 							<span class="icon-info-title">7.5 Million Registered Users</span>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 icon-col">
+						<div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 icon-col">
 							<div>
 								<img class="icon-img" src="/medias/icons/easyReturn.png" alt="">
 							</div>
 							<span class="icon-info-title">Easy Return Policy</span>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 icon-col">
+						<div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 icon-col">
 							<div>
 								<img class="icon-img" src="/medias/icons/shippingInfo.png" alt="">
 							</div>
@@ -46,62 +46,110 @@
 			<div class="footer-middle">
 				<div class="container-fluid">
 					<div class="row w-100 m-auto middle-row text-center">
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 middle-col">
-							<div>
-								<h6>WE ARE LOCATED AT –</h6>
-								<p class="text-justify">
-									<b>Store#1:-</b>
-									Jamuna Future Park, Shop#26-31, B#B, Level-1, North Court, Bashundhara, Dhaka. <br>
-									<b>Store#2:-</b> Grand Zam Zam Tower, Shop#501-503, Level-4, Sonargaon-Janapath Road, Sector-13, Uttara, Dhaka.
-								</p>
+						<div class="col-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
+							<div class="row w-100">
+								<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 middle-col">
+									<div>
+										<h6>WE ARE LOCATED AT –</h6>
+										<hr class="text-center w-50">
+										<p class="text-justify">
+											<b>Store#1:-</b>
+											Jamuna Future Park, Shop#26-31, B#B, Level-1, North Court, Bashundhara, Dhaka. <br>
+											<b>Store#2:-</b> Grand Zam Zam Tower, Shop#501-503, Level-4, Sonargaon-Janapath Road, Sector-13, Uttara, Dhaka.
+										</p>
+									</div>
+								</div>
+								<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 middle-col">
+									<div class="company-info">
+										<h6>COMPANY INFO</h6>
+										<hr class="text-center w-50">
+										<ul>
+											<li>
+												<a href="">About Us</a>
+											</li>
+											<li>
+												<a href="">Testimonials</a>
+											</li>
+											<li>
+												<a href="">Term & Conditions</a>
+											</li>
+											<li>
+												<a href="">Contact Us</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 middle-col">
+									<div class="supports">
+										<h6>SUPPORTS</h6>
+										<hr class="text-center w-50">
+										<ul>
+											<li>
+												<a href="">How to Order</a>
+											</li>
+											<li>
+												<a href="">Privacy Policy</a>
+											</li>
+											<li>
+												<a href="">Refund & Return Policy</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 middle-col">
+									<div class="my-account">
+										<h6>MY ACCOUNT</h6>
+										<hr class="text-center w-50">
+										<ul>
+											<li>
+												<a href="">My Order</a>
+											</li>
+											<li>
+												<a href="">My Wishlist</a>
+											</li>
+											<li>
+												<a href="">My Address</a>
+											</li>
+										</ul>
+									</div>
+								</div>
 							</div>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 middle-col">
-							<div class="company-info">
-								<h6>COMPANY INFO</h6>
-								<ul>
-									<li>
-										<a href="">About Us</a>
-									</li>
-									<li>
-										<a href="">Testimonials</a>
-									</li>
-									<li>
-										<a href="">Term & Conditions</a>
-									</li>
-									<li>
-										<a href="">Contact Us</a>
-									</li>
-								</ul>
+						<div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 social-col">
+							<div class="w-100 m-auto social-connect">
+								<div align="center" class="socialbtns w-100">
+									<h6 class="text-center">CONNECT WITH US</h6>
+									<!-- <hr class="text-center w-50"> -->
+									<ul class="d-flex w-100 justify-content-center">
+										<li><a href="" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
+										<li><a href="" class="youtube"><i class="fab fa-youtube"></i></a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="w-100 m-auto newsletter">
+								<h6 class="text-center">Newsletter</h6>
+								<!-- <hr class="text-center w-50"> -->
+								<form action="">
+									<div class="input-group">
+										<input type="text" class="form-control text-center" placeholder="Your Email Address" aria-label="Your Email Address" aria-describedby="basic-addon2">
+										<div class="input-group-append">
+											<button type="submit" class="input-group-text" id="basic-addon2"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+										</div>
+									</div>
+								</form>
 							</div>
 						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 middle-col">
-							<div>
-								<img class="icon-img" src="/medias/icons/brandNumber.png" alt="">
+					</div>
+					<div class="w-100 m-auto">
+						<div class="we-accept">
+							<div class="w-100 text-center">
+								<img class="w-75" src="medias/img/pay-with.webp" alt="">
 							</div>
-							<span class="icon-info-title">5800 Brands</span>
-						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 middle-col">
-							<div>
-								<img class="icon-img" src="/medias/icons/brandNumber.png" alt="">
-							</div>
-							<span class="icon-info-title">5800 Brands</span>
-						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 middle-col">
-							<div>
-								<img class="icon-img" src="/medias/icons/brandNumber.png" alt="">
-							</div>
-							<span class="icon-info-title">5800 Brands</span>
-						</div>
-						<div class="col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2 middle-col">
-							<div>
-								<img class="icon-img" src="/medias/icons/brandNumber.png" alt="">
-							</div>
-							<span class="icon-info-title">5800 Brands</span>
 						</div>
 					</div>
 				</div>
 			</div>
+			<p class="copyright mb-auto">Copyright ©{{ new Date().getFullYear() }} KidsParadiseBD. All Rights Reserved. A concern of Radiant International. Developed by: <a href="https://coreitechs.com/" target="_blank">CoreITechs</a> coreitechs</p>
 		</div>
 	</div>
 </template>
